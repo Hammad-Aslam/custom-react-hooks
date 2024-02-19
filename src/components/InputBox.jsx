001c12b55ac75e12f888dd235a751aca0ae9ae4a
Hammad <hammadaslam382@gmail.com>
2024-02-19 15:11:42 +0500

    complete

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,3 +1,5 @@
+import React, { useId } from "react";
+
 function InputBox({
   label,
   amount,
@@ -7,14 +9,21 @@ function InputBox({
   selectCurrency = "usd",
   amountDisable = false,
   currencyDisable = false,
-
   className = "",
 }) {
+  const amountInputId = useId();
+
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label
+          htmlFor={amountInputId}
+          className="text-black/40 mb-2 inline-block"
+        >
+          {label}
+        </label>
         <input
+          id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
@@ -30,14 +39,12 @@ function InputBox({
         <select
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={selectCurrency}
-          onChange={(e) =>
-            onCurrencyChange && onCurrencyChange(Number(e.target.value))
-          }
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           disabled={currencyDisable}
         >
           {currencyOptions.map((currency) => (
             <option key={currency} value={currency}>
-              usd
+              {currency}
             </option>
           ))}
         </select>
--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,15 +1,17 @@
-const { useEffect, useState } = require("react");
+import { useEffect, useState } from "react";
 
-function useInfoCurrency(currency) {
-  let [data, setData] = useState({});
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
   useEffect(() => {
     fetch(
       `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
     )
       .then((res) => res.json())
-      .then((res) => setData[currency]);
+      .then((res) => setData(res[currency]));
+    console.log(data);
   }, [currency]);
+  console.log(data);
   return data;
 }
 
-export default useInfoCurrency;
+export default useCurrencyInfo;
